47049b33f40aaf5150f3de5c28c61f57d2977c39
SLILAM Anwar <slilamanouar@gmail.com>
2020-12-13 15:33:49 +0100

    initial commit

---
+++ WindowsFormsWCF/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsWCF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labelx = new System.Windows.Forms.Label();
+            this.labely = new System.Windows.Forms.Label();
+            this.txtx = new System.Windows.Forms.TextBox();
+            this.txty = new System.Windows.Forms.TextBox();
+            this.txtz = new System.Windows.Forms.TextBox();
+            this.labelz = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(274, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(409, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(529, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // labelx
+            // 
+            this.labelx.AutoSize = true;
+            this.labelx.Location = new System.Drawing.Point(173, 101);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(12, 13);
+            this.labelx.TabIndex = 4;
+            this.labelx.Text = "x";
+            this.labelx.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labely
+            // 
+            this.labely.AutoSize = true;
+            this.labely.Location = new System.Drawing.Point(173, 150);
+            this.labely.Name = "labely";
+            this.labely.Size = new System.Drawing.Size(12, 13);
+            this.labely.TabIndex = 5;
+            this.labely.Text = "y";
+            // 
+            // txtx
+            // 
+            this.txtx.Location = new System.Drawing.Point(274, 101);
+            this.txtx.Name = "txtx";
+            this.txtx.Size = new System.Drawing.Size(100, 20);
+            this.txtx.TabIndex = 6;
+            // 
+            // txty
+            // 
+            this.txty.Location = new System.Drawing.Point(274, 150);
+            this.txty.Name = "txty";
+            this.txty.Size = new System.Drawing.Size(100, 20);
+            this.txty.TabIndex = 7;
+            // 
+            // txtz
+            // 
+            this.txtz.Location = new System.Drawing.Point(274, 192);
+            this.txtz.Name = "txtz";
+            this.txtz.Size = new System.Drawing.Size(100, 20);
+            this.txtz.TabIndex = 8;
+            // 
+            // labelz
+            // 
+            this.labelz.AutoSize = true;
+            this.labelz.Location = new System.Drawing.Point(173, 192);
+            this.labelz.Name = "labelz";
+            this.labelz.Size = new System.Drawing.Size(21, 13);
+            this.labelz.TabIndex = 9;
+            this.labelz.Text = "res";
+            this.labelz.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelz);
+            this.Controls.Add(this.txtz);
+            this.Controls.Add(this.txty);
+            this.Controls.Add(this.txtx);
+            this.Controls.Add(this.labely);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label labelx;
+        private System.Windows.Forms.Label labely;
+        private System.Windows.Forms.TextBox txtx;
+        private System.Windows.Forms.TextBox txty;
+        private System.Windows.Forms.TextBox txtz;
+        private System.Windows.Forms.Label labelz;
+    }
+}
+
---
+++ WindowsFormsWCF/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsWCF
+{
+    public partial class Form1 : Form
+    {
+        WCFService1.ServiceClient cal = new WCFService1.ServiceClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtz.Text = Convert.ToString(cal.WebAdd(int.Parse(txtx.Text), int.Parse(txty.Text)));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtz.Text = Convert.ToString(cal.WebSoustraction(int.Parse(txtx.Text), int.Parse(txty.Text)));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtz.Text = Convert.ToString(cal.WebMultiply(int.Parse(txtx.Text), int.Parse(txty.Text)));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtz.Text = Convert.ToString(cal.WebDivision(int.Parse(txtx.Text), int.Parse(txty.Text)));
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
